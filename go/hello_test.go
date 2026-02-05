19dfa748812b9b7b67a72d07a393a936e53c793a
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2026-02-05 15:16:58 -0800

    Refactor tests

--- go/hello_test.go
+++ go/hello_test.go
@@ -16,17 +16,28 @@ func TestHello(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("\nGot: %q \n Wanted: %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
-		want := "Hello, World"
+		want := "Hello, World!"
 
-		if got != want {
-			t.Errorf("\nGot: %q \n Wanted: %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// Adding this line tells the compiler, hey i am just
+	// a helper and when the test fails please only
+	// report the line inside the actual test that cause the failure
+	// and not the line in me that cause it.
+	//
+	// So, in the case of TestHello it will report the assertCorrectMessage
+	// failed instead of oh t.Errorf was called.
+	t.Helper()
+
+	if got != want {
+		t.Errorf("\nGot: %q \n Wanted: %q", got, want)
+	}
 }

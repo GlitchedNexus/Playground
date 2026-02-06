c876448e42add777768c0b5e90ebd9d825a65e9a
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2026-02-05 20:13:52 -0800

    Arrays

--- go/arrays_and_slices/aas.go
+++ go/arrays_and_slices/aas.go
@@ -8,3 +8,53 @@ func Sum(nums []int) int {
 
 	return sum
 }
+
+// This is a variadic function that can
+// take a variable number of arguments.
+func SumAll(numbersToSum ...[]int) []int {
+
+	// Make let us make a slice with a starting
+	// capacity of the length of the numbersToSum
+	// we need to work through.
+	//
+	// The length of a slice is the number of elements
+	// it holds measure by len(mySlice).
+	//
+	// The capacity onf the other hand is the
+	// number of elements the slice can hold in the underlying
+	// array by cap(mySlice).
+	// You can index slices just like you would with arrays
+	//
+	// Implementation One:
+	// length := len(numbersToSum)
+	// sums := make([]int, length)
+
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- go/arrays_and_slices/aas_test.go
+++ go/arrays_and_slices/aas_test.go
@@ -1,6 +1,10 @@
 package arraysandslices
 
-import "testing"
+import (
+	"reflect"
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Summing all ints in a collection of len 5.", func(t *testing.T) {
@@ -27,4 +31,44 @@ func assetCorrectResult(t *testing.T, got, want int, numbers []int) {
 	if got != want {
 		t.Errorf("\n Got: %d \n Wanted: %d \n Given: %v", got, want, numbers)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{3, 4, 5})
+	want := []int{3, 12}
+
+	// Go does not let you use equality operators with slices.
+	//
+	// Since we can't use ==, we'll use reflect.DeepEqual
+	// which is a recursive relaxation of Go's == operator.
+	//
+	// We can also use the Equal() function from the
+	// slices package itself.
+	if !slices.Equal(got, want) {
+		t.Errorf("\n Got: %v \n Wanted: %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("\n Got: %v \n Wanted: %v", got, want)
+		}
+	}
+
+	t.Run("Sum of non-empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4, 5})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("Sum of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{})
+		want := []int{}
+
+		checkSums(t, got, want)
+	})
 }

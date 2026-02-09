113ef3e4d05f600e814c00884647a52c6b46344e
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2026-02-08 22:34:48 -0800

    message passing

--- erlang/problems/message_passing.erl
+++ erlang/problems/message_passing.erl
@@ -1,6 +1,6 @@
 -module(message_passing).
 
--export([add_proc/1, adder/0, acc_proc/1, accumulator/0]).
+-export([add_proc/1, adder/0, acc_proc/1, accumulator/0, acc_stack/1]).
 
 add_proc(PPid) ->
     receive
@@ -18,13 +18,25 @@ adder() ->
 acc_proc(Tally) when is_integer(Tally) ->
     receive
         N when is_integer(N) ->
-            [acc_proc(Tally + N)];
+            acc_proc(Tally + N);
         {Pid, total} ->
             Pid ! Tally,
-            {acc_proc(Tally)};
+            acc_proc(Tally);
         exit ->
             Tally
     end.
 
 accumulator() ->
     spawn(fun() -> acc_proc(0) end).
+
+acc_stack(N) ->
+    AccPid = accumulator(),
+    [AccPid ! I || I <- lists:seq(1, N)],
+    AccPid ! {self(), total},
+    receive
+        Tally ->
+            Tally
+    end,
+    {stack_size, Size} = process_info(AccPid, stack_size),
+    AccPid ! exit,
+    io:format("N=~b, stack size = ~b, Tally = ~b~n", [N, Size, Tally]).

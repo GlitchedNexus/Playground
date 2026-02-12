f4060ad91c77be98cc29edc0d9a1b6543c403dad
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2026-02-11 20:59:04 -0800

    maps

--- go/maps/maps.go
+++ go/maps/maps.go
@@ -1,1 +1,20 @@
 package maps
+
+import "errors"
+
+var ErrorKeyNotFound = errors.New("Key not found")
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(key string) (string, error) {
+	val, ok := d[key]
+	if !ok {
+		return "", ErrorKeyNotFound
+	}
+
+	return val, nil
+}
+
+func (d Dictionary) Add(key, value string) {
+	d[key] = value
+}
--- go/maps/maps_test.go
+++ go/maps/maps_test.go
@@ -1,1 +1,55 @@
 package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+
+	t.Run("key exists in dictionary", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
+
+		key := "test"
+		got, _ := dictionary.Search(key)
+		want := "this is just a test"
+
+		assertStrings(t, got, want, key)
+	})
+
+	t.Run("key not in the dictionary", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
+
+		key := "abrcadabra"
+		_, got := dictionary.Search(key)
+
+		if got == nil {
+			t.Fatal("Expected error got nil")
+		}
+
+		assertError(t, got, ErrorKeyNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		dictionary := Dictionary{}
+		dictionary.Add("Key One", "Value One")
+
+		got, _ := dictionary.Search("Key One")
+		want := "Value One"
+
+		assertStrings(t, got, want, "Key One")
+	})
+}
+
+func assertStrings(t testing.TB, got, want, key string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("\nGot: %q\nWant: %q\nGiven: %q", got, want, key)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("\nGot: %q\nWant: %q\n", got, want)
+	}
+}

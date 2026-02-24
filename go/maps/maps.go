fe0c74ffee36d89b2e87c9858efa3e2e383bf2c5
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2026-02-23 19:27:14 -0800

    MAPS

--- go/maps/maps.go
+++ go/maps/maps.go
@@ -1,10 +1,18 @@
 package maps
 
-import "errors"
-
-var ErrorKeyNotFound = errors.New("Key not found")
+const (
+	ErrorKeyNotFound     = DictionaryErr("Key not found")
+	ErrWordExists        = DictionaryErr("Key already exists")
+	ErrorKeyDoesNotExist = DictionaryErr("Key you are trying to update doesn't exist.")
+)
 
 type Dictionary map[string]string
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 // An interesting property of maps is that you can
 // modify them without passing as an address to
@@ -46,6 +54,46 @@ func (d Dictionary) Search(key string) (string, error) {
 	return val, nil
 }
 
-func (d Dictionary) Add(key, value string) {
-	d[key] = value
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrorKeyNotFound:
+		d[key] = value
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(key, newValue string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrorKeyNotFound:
+		return ErrorKeyDoesNotExist
+	case nil:
+		d[key] = newValue
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(key string) (bool, error) {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrorKeyNotFound:
+		return false, ErrorKeyNotFound
+	case nil:
+		delete(d, key)
+		return true, nil
+	default:
+		return false, err
+	}
 }
--- go/maps/maps_test.go
+++ go/maps/maps_test.go
@@ -33,16 +33,89 @@ func TestAdd(t *testing.T) {
 		dictionary := Dictionary{}
 		key := "Key One"
 		value := "Value One"
-		dictionary.Add(key, value)
+		err := dictionary.Add(key, value)
 
+		assertError(t, err, nil)
+		assertValue(t, dictionary, key, value)
+	})
+
+	t.Run("", func(t *testing.T) {
+		key := "test"
+		value := "this is just a test"
+		dictionary := Dictionary{key: value}
+		err := dictionary.Add(key, "new test")
+
+		assertError(t, err, ErrWordExists)
 		assertValue(t, dictionary, key, value)
 	})
 }
+
+func TestUpdate(t *testing.T) {
+	t.Run("test update where key is in the map", func(t *testing.T) {
+		key := "Key One"
+		value := "Value One"
+		dictionary := Dictionary{key: value}
+		newValue := "New Value"
+
+		err := dictionary.Update(key, newValue)
+
+		assertError(t, err, nil)
+		assertValue(t, dictionary, key, newValue)
+	})
+
+	t.Run("test update where key is not in the map", func(t *testing.T) {
+		key := "test"
+		value := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(key, value)
+
+		assertError(t, err, ErrorKeyDoesNotExist)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("Delete key in the map", func(t *testing.T) {
+		key := "Key One"
+		value := "Value One"
+		dictionary := Dictionary{key: value}
+
+		success, err := dictionary.Delete(key)
+
+		assertEquals(t, success, true)
+		assertError(t, err, nil)
+
+		_, err = dictionary.Search(key)
+		assertError(t, err, ErrorKeyNotFound)
+	})
+
+	t.Run("Delete key not in the map", func(t *testing.T) {
+		key := "Key One"
+		dictionary := Dictionary{}
+
+		success, err := dictionary.Delete(key)
+
+		assertEquals(t, success, false)
+		assertError(t, err, ErrorKeyNotFound)
+
+	})
+}
+
+// ========================================================================
+// +							Helpers									  +
+// ========================================================================
 
 func assertStrings(t testing.TB, got, want, key string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("\nGot: %q\nWant: %q\nGiven: %q", got, want, key)
+	}
+}
+
+func assertEquals(t testing.TB, got, want any) {
+	t.Helper()
+	if got != want {
+		t.Errorf("\nGot: %q\nWant: %q\n", got, want)
 	}
 }
 

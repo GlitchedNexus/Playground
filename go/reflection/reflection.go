96a409c6f5fbba884463e4800971c120115be54a
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2026-03-01 03:16:31 -0800

    Finish refelction

--- go/reflection/reflection.go
+++ go/reflection/reflection.go
@@ -7,8 +7,51 @@ import "reflect"
 // it's a form of metaprogramming. It's also a great source
 // of confusion.
 
-func walk(x interface{}, fn func(string)) {
+func walk(x any, fn func(string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for {
+			if v, ok := val.Recv(); ok {
+				walkValue(v)
+			} else {
+				break
+			}
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
+		}
+	}
+
+}
+
+func getValue(x any) reflect.Value {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
 }

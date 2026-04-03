8d566cfa1793bf53f7fd57420894c4ae554ee6eb
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2026-04-02 23:02:10 -0700

    Templating

--- go/templating/blogrenderer.go
+++ go/templating/blogrenderer.go
@@ -1,6 +1,9 @@
 package blogrenderer
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Post struct {
 	Title       string
@@ -10,5 +13,30 @@ type Post struct {
 }
 
 func Render(w io.Writer, post Post) error {
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>\n<p>%s</p>", post.Title, post.Description)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "\nTags: <ul>")
+
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range post.Tags {
+		_, err = fmt.Fprintf(w, "<li>%s</li>", tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprintf(w, "</ul>")
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
